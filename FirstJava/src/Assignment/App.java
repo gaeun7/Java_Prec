248afb9313f95631ed7eaa3dd65a04dede853700
gaeun7 <rkdms1111111@naver.com>
2024-04-23 19:59:12 +0900

    4-23 개인 과제 level1 3번째 요구사항 (1차 시도 - .)

--- FirstJava/src/Assignment/App.java
+++ FirstJava/src/Assignment/App.java
@@ -9,13 +9,40 @@ public class App {
         int firstnum = sc.nextInt();
         System.out.println("두 번째 숫자를 입력하세요:  ");
         int secondnum = sc.nextInt();
+        if (firstnum <= 0 || secondnum <= 0){
+            System.out.println("양의 정수를 입력해주세요.");
+        }
 
         sc.nextLine();
 
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.nextLine().charAt(0);
 
-        System.out.println("첫 번째 숫자: " + firstnum + "두 번째 숫자: " + secondnum + "연산기호: " + operator); // 입력 받은 값이 변수에 잘 저장 되었는지 확인
+        int result = 0;
 
+        switch (operator){
+            case '+':
+                result = firstnum + secondnum;
+                System.out.println("결과: " + result);
+                break;
+            case '-':
+                result = firstnum - secondnum;
+                System.out.println("결과: " + result);
+                break;
+            case '*':
+                result = firstnum * secondnum;
+                System.out.println("결과: " + result);
+                break;
+            case '/':
+                if (secondnum > 0) {
+                    result = firstnum / secondnum;
+                    System.out.println("결과: " + result);
+                    break;
+                }
+                else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0을 입력할 수 없습니다.");
+                }
+            default:
+        }
     }
 }
