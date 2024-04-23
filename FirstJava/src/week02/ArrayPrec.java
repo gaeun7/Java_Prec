172546f06774062755b1326de7f1a0feffa41ced
gaeun7 <rkdms1111111@naver.com>
2024-04-23 11:05:41 +0900

    4-23일 자바 강의 연습

---
+++ FirstJava/src/week02/ArrayPrec.java
@@ -0,0 +1,22 @@
+package week02;
+
+public class ArrayPrec {
+    public static void main(String[] args) {
+        // 배열 선언
+        // 일반적인 선언방식
+        int[] intArray;    // 정수 배열
+        long[] longArray;
+        double[] doubleArray;  // 실수 배열
+        char[] charArray;  // 문자 배열
+        String[] stringArray; // 문자열 배열
+
+        // 배열 선언
+        // 2번째 선언방식
+        int IntArray[];    // 정수 배열 , 위의 변수 명과 겹치지 않도록 대문자로 시작 (위외 소문자처럼 사용하기.)
+        long LongArray[];
+        double DoubleArray[];  // 실수 배열
+        char CharArray[];  // 문자 배열
+        String StringArray[]; // 문자열 배열
+
+    }
+}
