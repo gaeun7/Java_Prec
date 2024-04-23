0a9d8b1a23e0f7dc23ade01615bdfab81acf65cc
gaeun7 <rkdms1111111@naver.com>
2024-04-23 19:35:59 +0900

    4-23 개인 과제 level1 2번째 요구사항 (가장 직관적으로 입력 받고 저장.)

--- FirstJava/src/Assignment/App.java
+++ FirstJava/src/Assignment/App.java
@@ -1,5 +1,4 @@
 package Assignment;
-
 import java.util.Scanner;
 
 public class App {
@@ -14,7 +13,7 @@ public class App {
         sc.nextLine();
 
         System.out.println("사칙연산 기호를 입력하세요: ");
-        String operator = String.valueOf(sc.nextLine().charAt(0));
+        char operator = sc.nextLine().charAt(0);
 
         System.out.println("첫 번째 숫자: " + firstnum + "두 번째 숫자: " + secondnum + "연산기호: " + operator); // 입력 받은 변수가 잘 저장 되었는지 확인
 
