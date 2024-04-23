792e27194c455f1f84bf432af4f60c0ba9489be4
gaeun7 <rkdms1111111@naver.com>
2024-04-23 20:15:45 +0900

    4-23 개인 과제 level1 4번째 요구사항

--- FirstJava/src/Assignment/App.java
+++ FirstJava/src/Assignment/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        while (true){
         System.out.println("첫 번째 숫자를 입력하세요: ");
         int firstnum = sc.nextInt(); //첫 번째 숫자
         System.out.println("두 번째 숫자를 입력하세요:  ");
@@ -23,7 +24,7 @@ public class App {
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.nextLine().charAt(0);
 
-        switch (operator){
+        switch (operator) {
             case '+': // 덧셈 연산
                 result = firstnum + secondnum;
                 System.out.println("결과: " + result);
@@ -41,11 +42,15 @@ public class App {
                     result = firstnum / secondnum;
                     System.out.println("결과: " + result);
                     break;
-                }
-                else {
+                } else {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0을 입력할 수 없습니다.");
                 }
             default:
+            }
+            System.out.println("더 게산하시겠습니까? (exit 입력 시 종료)");
+            String text = sc.nextLine();
+            if (text.equals("exit"))
+                break;
         }
     }
 }
