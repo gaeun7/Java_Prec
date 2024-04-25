8d695d9466fbf57904b4514ba86cf5f40d0be1db
gaeun7 <rkdms1111111@naver.com>
2024-04-25 16:29:43 +0900

    4-25 개인과제 Level 1 5번째 요구사항

--- FirstJava/src/Assignment/App.java
+++ FirstJava/src/Assignment/App.java
@@ -3,48 +3,65 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        int[] intArray = new int[10];
+        int count = 0;
+
         Scanner sc = new Scanner(System.in);
 
-        while (true){
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int firstnum = sc.nextInt(); //첫 번째 숫자
-        System.out.println("두 번째 숫자를 입력하세요:  ");
-        int secondnum = sc.nextInt(); // 두 번째 숫자
+        while (true) {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            int firstNum = sc.nextInt(); //첫 번째 숫자
+            System.out.println("두 번째 숫자를 입력하세요:  ");
+            int secondNum = sc.nextInt(); // 두 번째 숫자
 
-        sc.nextLine(); // 개행 문자 소비
+            sc.nextLine(); // 개행 문자 소비
 
-        int result = 0; // 초기화
+            int result = 0; // 초기화
 
-        System.out.println("사칙연산 기호를 입력하세요: ");
-        char operator = sc.nextLine().charAt(0);
+            System.out.println("사칙연산 기호를 입력하세요: ");
+            char operator = sc.nextLine().charAt(0); // charAt() : String 타입의 데이터(문자열)에서 특정 문자를 char 타입으로 변환할 때 사용하는 함수
 
-        switch (operator) {
-            case '+': // 덧셈 연산
-                result = firstnum + secondnum;
-                System.out.println("결과: " + result);
-                break;
-            case '-': // 뺄셈 연산
-                result = firstnum - secondnum;
-                System.out.println("결과: " + result);
-                break;
-            case '*': // 곱셈 연산
-                result = firstnum * secondnum;
-                System.out.println("결과: " + result);
-                break;
-            case '/': // 나눗셈 연산
-                if (secondnum > 0) {
-                    result = firstnum / secondnum;
+            switch (operator) {
+                case '+': // 덧셈 연산
+                    result = firstNum + secondNum;
                     System.out.println("결과: " + result);
                     break;
-                } else {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0을 입력할 수 없습니다.");
-                }
-            default:
+                case '-': // 뺄셈 연산
+                    result = firstNum - secondNum;
+                    System.out.println("결과: " + result);
+                    break;
+                case '*': // 곱셈 연산
+                    result = firstNum * secondNum;
+                    System.out.println("결과: " + result);
+                    break;
+                case '/': // 나눗셈 연산
+                    if (secondNum > 0) {
+                        result = firstNum / secondNum;
+                        System.out.println("결과: " + result);
+                        break;
+                    } else { //분모에 0을 입력한 경우
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0을 입력할 수 없습니다.");
+                    }
+                    break; 
+                default: 
+                    System.out.println("잘못된 연산자입니다.");
             }
-            System.out.println("더 게산하시겠습니까? (exit 입력 시 종료)");
+            
+            if (count < 10) {  //10번 이하의 연산 결과값일 때
+                intArray[count++] = result;
+            } else { //배열 크기를 초과한 경우
+                System.out.println("더 이상 결과를 저장할 수 없습니다.");
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String text = sc.nextLine();
             if (text.equals("exit"))
                 break;
+        }
+        
+        System.out.println("저장된 결과값:");  // 배열에 저장된 결과값 확인하기
+        for (int i = 0; i < count; i++) {
+            System.out.println(intArray[i]);
         }
     }
 }
