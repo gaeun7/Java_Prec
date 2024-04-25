4c2e23aa83b24ee8672bdc9919322643b8481e89
gaeun7 <rkdms1111111@naver.com>
2024-04-25 17:33:20 +0900

    4-25 개인과제 Level 1 6번째 요구사항 (2차 시도_실패(┬┬﹏┬┬))

--- FirstJava/src/Assignment/App.java
+++ FirstJava/src/Assignment/App.java
@@ -6,54 +6,58 @@ public class App {
     public static void main(String[] args) {
         int[] intArray = new int[10];
         int count = 0;
+        int index = 0;
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("첫 번째 숫자를 입력하세요: ");
-            int firstNum = sc.nextInt(); //첫 번째 숫자
+            int firstNum = sc.nextInt();
             System.out.println("두 번째 숫자를 입력하세요:  ");
-            int secondNum = sc.nextInt(); // 두 번째 숫자
+            int secondNum = sc.nextInt();
 
             sc.nextLine(); // 개행 문자 소비
 
-            int result = 0; // 초기화
+            int result = 0;
 
             System.out.println("사칙연산 기호를 입력하세요: ");
-            char operator = sc.nextLine().charAt(0); // charAt() : String 타입의 데이터(문자열)에서 특정 문자를 char 타입으로 변환할 때 사용하는 함수
+            char operator = sc.nextLine().charAt(0);
 
             switch (operator) {
-                case '+': // 덧셈 연산
+                case '+':
                     result = firstNum + secondNum;
                     System.out.println("결과: " + result);
                     break;
-                case '-': // 뺄셈 연산
+                case '-':
                     result = firstNum - secondNum;
                     System.out.println("결과: " + result);
                     break;
-                case '*': // 곱셈 연산
+                case '*':
                     result = firstNum * secondNum;
                     System.out.println("결과: " + result);
                     break;
-                case '/': // 나눗셈 연산
+                case '/':
                     if (secondNum > 0) {
                         result = firstNum / secondNum;
                         System.out.println("결과: " + result);
-                        break;
-                    } else { //분모에 0을 입력한 경우
+                    } else {
                         System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0을 입력할 수 없습니다.");
                     }
-                    break; 
-                default: 
+                    break;
+                default:
                     System.out.println("잘못된 연산자입니다.");
             }
-            
-            if (count < 10) {  //10번 이하의 연산 결과값일 때
-                intArray[count++] = result;
-            } else if (count >= 10) { //배열 크기를 초과한 경우
-              intArray[0] = Integer.parseInt(null);       //0번째 인덱스 값을 없앰
-              int[] copyOfInt = Arrays.copyOf(intArray, intArray.length - 1);  //배열 값 재배치
-              intArray[count++] = result;  // 새로운 값 마지막 인덱스에 배치,,,
+
+            // Save result value and update index
+            if (count < 10) { // If less than 10 results saved, just increment index
+                intArray[index] = result;
+                count++;
+                index++;
+            } else { // If 10 results already saved
+                if (index == 10) // If index is at the end, reset it to 0
+                    index = 0;
+                intArray[index] = result; // Save new result at the last index
+                index++; // Move to next index
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
@@ -61,8 +65,8 @@ public class App {
             if (text.equals("exit"))
                 break;
         }
-        
-        System.out.println("저장된 결과값:");  // 배열에 저장된 결과값 확인하기
+
+        System.out.println("저장된 결과값:");
         for (int i = 0; i < count; i++) {
             System.out.println(intArray[i]);
         }
